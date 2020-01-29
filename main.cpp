d12c2f1546a4a50c06e3159e42a91a58423becf5
matkatmusic <matkatmusic@gmail.com>
2020-01-28 19:51:22 -0800

    Added Part 2 instructions

--- main.cpp
+++ main.cpp
@@ -1,37 +1,72 @@
-#include <iostream>
-#include <limits>
-
 /*
- Chapter 2 Part 7
- Returning Values tasks
+ Project 4 - Part 2 / 9
+ Video: Chapter 3 Part 6
 
- 1) write 3 UDTs named FloatType, DoubleType, IntType.
+ Create a branch named Part2
 
- 2) give each UDT the following member functions:
-        add( lhs, rhs );
-        subtract( lhs, rhs );
-        multiply( lhs, rhs );
-        divide( lhs, rhs );
+New/This/Pointers/References conclusion
 
- 3) make them return the correct primitive type. e.g. if you're implementing the FloatType::add
- function, your implementation would start like this: float FloatType::add( float lhs, float rhs )
+    Build/Run often with this task to make sure you're not breaking the code with each step.
+    I recommend committing after you get each step working so you can revert to a working version easily if needed.
+ 
+ 0) in the blank space below, declare/define an empty struct named 'A' on a single Line. 
+     on the lines below it, write a struct named 'HeapA' that correctly shows how to own an instance of 'A' 
+         on the heap without leaking, without using smart pointers. 
+ */
 
- 4) declare a few instances of each class in main() and call the member functions with the
- appropriate arguments. FloatType ft; auto result = ft.add(3.2f, 23.f );
 
- 5) print out those results using std::cout:
-        std::cout "result of ft.add(): " << result << std::endl;
 
- 6) After you finish defining each type/function, click the [run] button.  Clear up any errors or
- warnings as best you can.
 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and
- click [Commit and push].
+
+
+
+
+
+
+
+ /*
+ 1) Edit your 3 structs so that they own a heap-allocated primitive type without using smart pointers  
+         IntType should own a heap-allocated int, for example.
+ 
+ 2) give it a constructor that takes the appropriate primitive
+    this argument will initialize the owned primitive's value.
+         i.e. if you're owning an int on the heap, your ctor argument will initialize that heap-allocated int's value.
+ 
+ 3) modify those add/subtract/divide/multiply member functions from chapter 2 on it
+         a) make them modify the owned numeric type
+         b) set them up so they can be chained together.
+             i.e.
+             DoubleType dt(3.5);
+             dt.add(3.0).multiply(-2.5).divide(7.2); //an example of chaining
+ 
+ 4) write some add/subtract/divide/multiply member functions for each type that take your 3 UDTs
+         for example, IntType::divide(const DoubleType& dt);
+ 
+ 5) print out the results with some creative couts 
+    i.e.
+         FloatType ft(0.1f);
+         IntType it(3);
+         std::cout << "adding 3 and subtracting 'it' from 'ft' results in the following value: " << *ft.add(2.f).subtract( it ).value << std::endl;  //note the dereference of the `value` member of `ft`
+ 
+ 6) Don't let your heap-allocated owned type leak!
+ 
+ 7) click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ Send me the the link to your repl.it in a DM on Slack
 
  Wait for my code review.
  */
 
-struct FloatType {
+
+
+
+#include <iostream>
+#include <limits>
+
+struct FloatType 
+{
     float add(float lhs, float rhs) { return lhs + rhs; }
 
     float subtract(float lhs, float rhs) { return lhs - rhs; }
@@ -48,7 +83,8 @@ struct FloatType {
     }
 };
 
-struct DoubleType {
+struct DoubleType 
+{
     double add(double lhs, double rhs) { return lhs + rhs; }
 
     double subtract(double lhs, double rhs) { return lhs - rhs; }
@@ -65,7 +101,8 @@ struct DoubleType {
     }
 };
 
-struct IntType {
+struct IntType 
+{
     int add(int lhs, int rhs) { return lhs + rhs; }
 
     int subtract(int lhs, int rhs) { return lhs - rhs; }
